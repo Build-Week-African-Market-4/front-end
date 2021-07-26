cbcbc4e48935bf27077f0dec52c06047d6afd1ce
JackOrth <jackorth19@gmail.com>
2021-07-26 12:31:34 -0600

    Completed Skeleton for items list on the home page.

--- market/src/App.js
+++ market/src/App.js
@@ -1,25 +1,15 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './Home'
 function App() {
+  const [items, setItems] = useState([])
   return (
-    <Home />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+  
+    <Route exact path='/'>
+    <Home items={items} />
+    </Route>
+    
   );
 }
 
