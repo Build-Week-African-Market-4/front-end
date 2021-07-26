ac7e68fc61b75f25bfd350280f96f8256c46ebf9
JackOrth <jackorth19@gmail.com>
2021-07-26 11:07:51 -0600

    Completed Home Page.

--- market/src/Home.js
+++ market/src/Home.js
@@ -1,9 +1,35 @@
-
+import React from 'react'
 
 export default function Home(){
     return(
-        <div>
-            <h1>Header</h1>
+        <div className="home">
+            <div className="home-header">
+                <nav>
+                    <h1>African Marketplace</h1>
+                    <div className="nav-links">
+                        <Link>Home</Link>
+                        <Link>Log In</Link>
+                        <Link>Items</Link>
+                    </div>
+                </nav>
+            </div>
+            <div className="home-body">
+                <div>
+                    <img 
+                    className=""
+                    src="https://media.istockphoto.com/photos/fruit-and-vegetable-market-picture-id160342236?k=6&m=160342236&s=612x612&w=0&h=JRs8uuwUJRTfVJKJWZd1OKd6-6zqHeUmgL7PzAqCeHs="
+                    alt=''
+                    />
+                </div>
+                <div className="home-buttons">
+                    <button>
+                        Log In
+                    </button>
+                    <button>
+                        Items
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
