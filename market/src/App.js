7a61731723fea4300772bd19b04da326ebb4c5cc
WillisLi <li.willis95@gmail.com>
2021-07-26 09:56:14 -0700

    set up login form, formScehma, and login states in app.js

--- market/src/App.js
+++ market/src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Login from './Login';
+import formSchema from './formSchema';
+import axios from 'axios';
+import { reach } from 'yup';
+
+const initialLoginValues = {
+  username: '',
+  password: '',
+}
+
+const initialLoginErrors = {
+  username: '',
+  password: '',
+}
 
 function App() {
+  const {logins, setLogins} = useState([])
+  const {loginValues, setLoginValues} = useState(initialLoginValues);
+  const {loginErrors, setLoginErrors} = useState(initialLoginErrors);
+  const [disabled, setDisabled] = useState(true);
+
+  const postLogin = newLogin => {
+    axios.post("#", newLogin)
+      .then(response => {
+          setLogins(response.data);
+      })
+      .catch(error => {
+          console.log(error);
+      })
+      .finally(() => {
+          setLoginValues(initialLoginValues);
+      })
+  }
+
+  const submitLogin = () => {
+      const newLogin = {
+          username: loginValues.username.trim(),
+          password: loginValues.password.trim(),
+      }
+      postLogin(newLogin);
+  }
+
+  const inputChange = (name, value) => {
+    validate(name, value)
+    setLoginValues({
+      ...loginValues,
+      [name]: value
+    })
+  }
+
+  useEffect(() => {
+    formSchema.isValid(loginValues).then(valid => setDisabled(!valid))
+  }, [loginValues])
+
+  const validate = (name, value) => {
+    reach(formSchema, name)
+      .validate(value)
+      .then(() => setLoginErrors({ ...loginErrors, [name]: '' }))
+      .catch(err => setLoginErrors({ ...loginErrors, [name]: err.errors[0]}))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "marketApp">      
+      <Route path = '/login'>
+          <Login
+              values = {loginValues}
+              login = {submitLogin}
+              input = {inputChange}
+              disabled = {disabled}
+              errors = {loginErrors}
+          />
+      </Route>
     </div>
   );
 }
---
+++ market/src/Login.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+function Login(props) {
+    const { values, login, change, disabled, errors, } = props;
+
+    const handleChange = event => {
+        const { name, value, type } = event.target;
+        change(name);
+    }
+
+    const handleLogin = event => {
+        event.preventDefault()
+        login()
+    }
+
+    return (
+        <form className = "login container" onSubmit = {handleLogin}>
+            <div className = "login inputs">
+                <label>Username
+                    <input
+                        value = {values.username}
+                        onChange= {handleChange}
+                        name = 'username'
+                        type = 'text'
+                    />
+                </label>
+
+                <label>Password
+                    <input
+                        value = {values.password}
+                        onChange = {handleChange}
+                        name = 'password'
+                        type = 'password'
+                    />
+                </label>         
+            </div>
+
+            <div className = "login submit">
+                <button disabled = {disabled}>Login</button>
+
+                <div className = 'errors'>
+                    <div>{errors.username}</div>
+                    <div>{errors.password}</div>
+                </div>
+            </div>
+        </form>
+    );
+}
+
+export default Login;
