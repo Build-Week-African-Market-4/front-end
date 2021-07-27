d12969d8a54cc392232575fbe760a9c96bbeeb5f
ashleyannlaz <paperpastels@gmail.com>
2021-07-26 22:40:39 -0400

    Fixed extra divs

--- market/src/Home.js
+++ market/src/Home.js
@@ -3,18 +3,15 @@ import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
 import img from './Assets/pic02.jpeg'
 
-// const Banner = styled.div`
-//     background-image: url(${img});
-//     height: 35vh;
-//     background-size: cover;
-//     background-position: center
-// `
-// const ItemsWrapper = styled.div`
-//     text-align: center;
-// `
-
-
-
+const Banner = styled.div`
+    background-image: url(${img});
+    height: 35vh;
+    background-size: cover;
+    background-position: center
+`
+const ItemsWrapper = styled.div`
+    text-align: center;
+`
 export default function Home(){
     
     const history = useHistory()
@@ -24,33 +21,18 @@ export default function Home(){
     
     return(
         <div className="home">
-            <div className="home-body">
-            {/* <Banner>
-                
-            </Banner> */}
-            </div>
-            {/* <ItemsWrapper> */}
+            <Banner>     
+            </Banner>
+            <ItemsWrapper>
                 <h3>See whats new.</h3>
-                <div>
 
-                    <img 
-                    className="body-image"
-                    src="https://media.istockphoto.com/photos/fruit-and-vegetable-market-picture-id160342236?k=6&m=160342236&s=612x612&w=0&h=JRs8uuwUJRTfVJKJWZd1OKd6-6zqHeUmgL7PzAqCeHs="
-                    alt=''
-                    />
-                </div>
                 <button
                     onClick={routeToItems}
                     className='items-button'
                     >
                     Items List
                     </button>
-            </div>
-            //         New Items Go Here
-            //     </div>
-            // </ItemsWrapper>            
-                
-
-        // </div>
+         </ItemsWrapper>            
+        </div>
     )
 }
