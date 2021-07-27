91f65c6e6cdc6f05990fdc6433ac575d67a4fe66
JackOrth <jackorth19@gmail.com>
2021-07-26 18:11:35 -0600

    Completed Items List Page and mapped each item from a fake API for now.

--- market/src/Home.js
+++ market/src/Home.js
@@ -1,39 +1,29 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
-export default function Home(props){
-    const { items } = props
+export default function Home(){
+    
+    const history = useHistory()
+    const routeToItems = () => {
+        history.push('items-list')
+    }
+    
     return(
         <div className="home">
             <div className="home-body">
                 <div>
                     <img 
-                    className=""
+                    className="body-image"
                     src="https://media.istockphoto.com/photos/fruit-and-vegetable-market-picture-id160342236?k=6&m=160342236&s=612x612&w=0&h=JRs8uuwUJRTfVJKJWZd1OKd6-6zqHeUmgL7PzAqCeHs="
                     alt=''
                     />
                 </div>
-                <div className="home-buttons">
-                    <button>
-                        Log In
-                    </button>
-                </div>
-            </div>
-            <div className="items-list">
-                <h2>Items</h2>
-                {items.map(item => (
-                    <div
-                    className="item-card"
-                    key={item.id}
+                <button
+                    onClick={routeToItems}
+                    className='items-button'
                     >
-                    <img 
-                    className="items-list-image"
-                    src={item.imageUrl}
-                    alt={item.name}
-                    />
-                    <p>{item.name}</p>
-                    <p>{item.price}</p>
-                    </div>
-                ))}
+                    Items List
+                    </button>
             </div>
         </div>
     )
