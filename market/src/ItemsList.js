fa51b857e6720fb6e9028bb6eaa03eae1c550e30
WillisLi <li.willis95@gmail.com>
2021-07-27 18:16:06 -0700

    changed price type to number in ItemForm and added a dropdown in ItemList, started work on a sort functionality

--- market/src/ItemsList.js
+++ market/src/ItemsList.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
 export default function ItemsList(props){
-    const { items } = props
+    const { items, sort } = props
+
+    const onSort = event => {
+        event.preventDefault();
+        sort();
+    }
+
     return(
         <div className='items-list'>
             <h2>Items List</h2>
+            <select
+                // id = 'sort-dropdown'
+                // onChange = {onSort}
+                // value = {values.sort}
+                // name = 'sort'
+            >
+                <option value = "">- Sort By -</option>
+                <option value = "first_name">Title</option>
+                <option value = "price">Price</option>
+            </select>
             {items.map(item => (
                 <div
                 className='item-card'
