a61525ff4d5ac22edf915c8868f52f83d68275d0
WillisLi <42555076+WillisLi@users.noreply.github.com>
2021-07-29 10:11:12 -0700

    Update schema.js

--- market/src/validation/schema.js
+++ market/src/validation/schema.js
@@ -1,11 +1,13 @@
 import * as Yup from 'yup';
 
 const schema = Yup.object().shape({
-    name: Yup
+    title: Yup
         .string()
         .trim()
-        .required('Product name is required')
+        .required('Product title is required')
         .min(3, 'Name must be at least 3 characters long'),
+    image: Yup
+        .string(),
     description: Yup
         .string()
         .trim()
